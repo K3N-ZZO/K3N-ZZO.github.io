273f01eb9ae75f861a6a17fc7290c0abe85478ed
K3N-ZZO <biercmichal@gmail.com>
2025-04-03 17:56:57 +0100

    Moved from localhost to render

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const apiRoot = 'https://taskapplication2.onrender.com/v1/tasks';
+  const trelloApiRoot = 'https://taskapplication2.onrender.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
